e43a8501f340650af9cb7a239d5856058ea87527
Afif Arman <afifarman50@gmail.com>
2025-08-06 15:25:19 +0700

    Refactor: change GPU to CPU max spec compatibility

--- src/services/handGestureService.js
+++ src/services/handGestureService.js
@@ -12,7 +12,7 @@ export const createHandLandmarker = async () => {
   handLandmarker = await HandLandmarker.createFromOptions(vision, {
     baseOptions: {
       modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
-      delegate: "GPU",
+      delegate: "CPU", //CPU (device spec kentang) | GPU (device spec non kentang)
     },
     runningMode: runningMode,
     numHands: 2, // Max number of hands to detect
